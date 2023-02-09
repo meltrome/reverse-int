8845f8d23a4d1b52f1954eaabe89e1ae948a04e6
Maksim <ilyshev@mail.ru>
2023-02-09 19:12:54 +0300

    feat: test 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+    let nToStr = n.toString().split('');
+    if (nToStr[0] === '-') {nToStr.shift();}
+    let revNum = nToStr.reverse().join("");
+    return revNum;
+};
